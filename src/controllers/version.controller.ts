5fa088bbff3500cc0f4c955c8a9273e7917da54e
i4nizer <iansandoval264@gmail.com>
2026-07-30 11:09:44 +0800

    Updated to version 0.0.28

--- src/controllers/version.controller.ts
+++ src/controllers/version.controller.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from "express"
 //
 
 const get: RequestHandler = async (req, res) => {
-    const version = "0.0.27"
+    const version = "0.0.28"
     const changes = ["In-App Update"]
     const required = true
     const url = "https://sgb-worker.iansandoval264.workers.dev/apk/v0.0.27/sgb.apk"
