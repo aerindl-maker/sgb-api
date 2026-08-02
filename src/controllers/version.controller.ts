bf19aac0759384e27b3ab2fe266cee66b377023c
i4nizer <iansandoval264@gmail.com>
2026-08-02 18:54:36 +0800

    Corrected url version.

--- src/controllers/version.controller.ts
+++ src/controllers/version.controller.ts
@@ -6,7 +6,7 @@ const get: RequestHandler = async (req, res) => {
     const version = "0.0.28"
     const changes = ["In-App Update"]
     const required = true
-    const url = "https://sgb-worker.iansandoval264.workers.dev/apk/v0.0.27/sgb.apk"
+    const url = "https://sgb-worker.iansandoval264.workers.dev/apk/v0.0.28/sgb.apk"
     return res.json({ url, version, changes, required })
 }
 
